316b318e8eef004e65d16ba7d20f83165d883f3d
Vani Ruchika Pabba <vaniruchika.pabba@gmail.com>
2023-08-24 17:07:06 -0400

    Added Shirt and Backdrop

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -14,7 +14,7 @@ const CustomButton = ({type,title,customStyles, handleClick}) => {
         }
     }
     return (
-    <button className='px-2 py-1.2 flex-1 rounded-md ${customStyles}'
+    <button className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
     style={generateStyle(type)} onClick={handleClick}
     >{title}</button>
   )
